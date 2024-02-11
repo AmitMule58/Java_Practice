75da5576b370d193691057196351a47443cd5a0d
Amit Mule <114135523+AmitMule58@users.noreply.github.com>
2024-02-12 00:29:35 +0530

    Add files via upload

---
+++ Star_Patterns/RightBcorner.java
@@ -0,0 +1,20 @@
+package Star_Patterns;
+
+public class RightBcorner {
+
+	public static void main(String[] args) 
+	{
+		for(int i=0; i<=4; i++) 
+		{
+			for(int j=4; j>=i; j--) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<=i; k++) 
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
