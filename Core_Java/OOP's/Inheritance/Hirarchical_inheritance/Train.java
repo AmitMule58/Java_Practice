57e1563314bdd73e615094fd6a7649e86bf08f55
Amit Mule <114135523+AmitMule58@users.noreply.github.com>
2024-02-17 21:32:42 +0530

    Delete Core_Java/OOP's directory

--- Core_Java/OOP's/Inheritance/Hirarchical_inheritance/Train.java
+++
@@ -1,7 +0,0 @@
-package Hirarchical_inheritance;
-
-public class Train extends Toys_Parent{
-void train() {
-	System.out.println("Train is 1st Child Class\n");
-}
-}
--- Core_Java/OOP's/Inheritance/Multilevel_Inheritance/Cat.java
+++
@@ -1,8 +0,0 @@
-package Multilevel_Inheritance;
-
-public class Cat extends Animal_Parent { //Sub Parent class
-
-	void cat() {
-		System.out.println("Cat whistles");
-	}
-}
